fe7cad95b5e5a6ccd9c360685e4ea03497e809f3
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2022-09-21 13:05:13 +0200

    Added routing with first main component - exchange rates table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExchangeTableComponent } from './exchange-table/exchange-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: ExchangeTableComponent },
+  { path: '**', component: ExchangeTableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ExchangeTableComponent } from './exchange-table/exchange-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ExchangeTableComponent
   ],
   imports: [
     BrowserModule,
