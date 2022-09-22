24fe54e09dd3decbce121e1d2a6ee4efdb857197
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2022-09-22 13:32:17 +0200

    Prepared simple table. Added data from Api.

--- src/app/exchange-table/exchange-table.component.ts
+++ src/app/exchange-table/exchange-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../service/config.service';
+import { CurrencyApi } from '../interface/interface'; 
 
 @Component({
   selector: 'app-exchange-table',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExchangeTableComponent implements OnInit {
 
-  constructor() { }
+  tableAAddress: string = 'https://api.nbp.pl/api/exchangerates/tables/A/?format=json';
+  rawExchangeTable: any;
+  currencyTable: Array<CurrencyApi> = [];
+
+  constructor(private service: ConfigService) {
+    this.service.serviceApi(this.tableAAddress).subscribe((res) => {
+      this.rawExchangeTable = res;
+      this.currencyTable = this.rawExchangeTable[0].rates;
+    })
+  }
 
   ngOnInit(): void {
   }
